7fb7dedd7b35c977b3016a203dca1a2c2ba77a55
lawalemma24 <lawalemma24@gmail.com>
2026-04-17 15:51:16 +0100

    server fixes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const profileRoutes = require('./route/profileRoute');
 
 require('dotenv').config();
 
-PORT = process.env.PORT || 7000;
+// PORT = process.env.PORT || 7000;
 
 const app = express();
 
@@ -21,8 +21,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/profiles', profileRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
